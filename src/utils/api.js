b5759ffb808137c2ab92bf4a0b45ee2ded13c83a
Collins Opiayo <opiayocollins2@gmail.com>
2025-07-21 19:57:20 +0300

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
-const BASE_URL = "https://smart-goal-manager.onrender.com/";
+const BASE_URL = "https://smart-goal-manager.onrender.com/goals";
 
 export const fetchGoals = async () => {
   const res = await fetch(BASE_URL);
+  if (!res.ok) throw new Error("Failed to fetch goals");
   return res.json();
 };
 
@@ -11,18 +12,22 @@ export const createGoal = async (goal) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(goal),
   });
+  if (!res.ok) throw new Error("Failed to create goal");
   return res.json();
 };
 
 export async function updateGoal(id, updatedData) {
-  const res = await fetch(`${BASE_URL}/goals/${id}`, {
+  const res = await fetch(`${BASE_URL}/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(updatedData),
   });
+  if (!res.ok) throw new Error("Failed to update goal");
   return res.json();
 }
 
 export const deleteGoal = async (id) => {
-  await fetch(`${BASE_URL}/${id}`, { method: "DELETE" });
+  const res = await fetch(`${BASE_URL}/${id}`, { method: "DELETE" });
+  if (!res.ok) throw new Error("Failed to delete goal");
+  return res.json();
 };
