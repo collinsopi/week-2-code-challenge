28f55302fe286790a52d8a568dba4293df97d40d
Collins Opiayo <opiayocollins2@gmail.com>
2025-07-21 09:03:56 +0300

    Prepare for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/week-2-code-challenge/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: '.', // will copy to dist/404.html
-          rename: '404.html',
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
+  base: '',
 });
