7bbae0b85675866023274f28746fcd2fa7a07b23
Collins Opiayo <opiayocollins2@gmail.com>
2025-07-22 11:24:22 +0300

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,22 +12,20 @@ export const createGoal = async (goal) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(goal),
   });
-  if (!res.ok) throw new Error("Failed to create goal");
   return res.json();
 };
 
-export async function updateGoal(id, updatedData) {
+export const updateGoal = async (id, updatedData) => {
   const res = await fetch(`${BASE_URL}/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(updatedData),
   });
-  if (!res.ok) throw new Error("Failed to update goal");
   return res.json();
-}
+};
 
 export const deleteGoal = async (id) => {
-  const res = await fetch(`${BASE_URL}/${id}`, { method: "DELETE" });
-  if (!res.ok) throw new Error("Failed to delete goal");
-  return res.json();
+  await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
 };
